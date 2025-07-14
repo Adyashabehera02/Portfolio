87a1c597707ffbea94d4c5f6d1f42ad317a32b14
Adyashabehera02 <adyashabehera02@gmal.com>
2025-07-14 16:43:03 +0530

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', loadComponent: () => import('./home/home').then(m => m.Home) },
+  { path: 'about', loadComponent: () => import('./about/about').then(m => m.About) },
+  { path: 'projects', loadComponent: () => import('./projects/projects').then(m => m.Projects) },
+  { path: 'resume', loadComponent: () => import('./resume/resume').then(m => m.Resume) },
+  { path: 'contact', loadComponent: () => import('./contact/contact').then(m => m.Contact) },
+];
